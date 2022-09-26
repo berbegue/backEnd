596bed9ba53647dc59fdff0318090378c34d6451
Bernardo Begue <ber.begue@outlook.com>
2022-09-26 20:10:51 -0300

    primer ejercicio

---
+++ 00-clases.js
@@ -0,0 +1,46 @@
+class Usuario{
+    constructor(nombre,apellido,libros=[{}],mascotas=[]){
+        this.name = nombre;
+        this.lastname = apellido;
+        this.books = libros;
+        this.pets = mascotas;
+    }
+    getFullName() {
+        console.log(`${this.name} ${this.lastname}`);
+    } 
+    addMascota(masc) {
+        this.pets.push(masc);
+    } 
+    countMascotas(){
+        console.log(this.pets.length);
+    }
+    addBook(nom,aut){
+        this.books.push({nombre: nom,autor: aut});
+    }
+    getBookNames(){
+        console.log(this.books.map((misLibros) => misLibros.nombre));
+    }
+    vermascotas() {
+        console.log(this.pets);
+        console.log(this.books);
+    } 
+    /*countMascotas(): 
+    addBook()
+    getBookNames()*/
+}
+const usuario1 = new Usuario('Bernardo', 'Begue Aliaga',[
+                                                    {
+                                                        nombre:'El padriono', 
+                                                        autor:'Mario Puzo'
+                                                    },
+                                                    {
+                                                        nombre:'El exorcista', 
+                                                        autor:'William P. Blatty'
+                                                    }
+                                                ],['Vittorio', 'Gatu'] );
+usuario1.getFullName();
+usuario1.addMascota('Romeo');
+usuario1.countMascotas();
+usuario1.addBook('Juan Salvador Gaviota','Richard Bach');
+usuario1.getBookNames();
+
